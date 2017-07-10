30f252e65fc7ee8b9b7b9fcedfcaafe64fea1c57
Aslak Knutsen <aslak@4fs.no>
2017-07-10 12:44:46 +0200

    Replace html/template with simple RegExp replace
    Avoids templates being html/xml safe escaped

--- openshift/process.go
+++ openshift/process.go
@@ -1,24 +1,21 @@
 package openshift
 
-import (
-	"bytes"
-	"html/template"
-	"regexp"
-)
+import "regexp"
 
 // Process takes a K8/Openshift Template as input and resolves the variable expresions
 func Process(source string, variables map[string]string) (string, error) {
-	target, err := template.New("openshift").Parse(replaceTemplateExpression(source))
-	if err != nil {
-		return "", err
-	}
-	var buf bytes.Buffer
-	err = target.Execute(&buf, variables)
-	if err != nil {
-		return "", err
-	}
-	str := buf.String()
-	return str, nil
+	reg := regexp.MustCompile(`\${([A-Z_]+)}`)
+	return string(reg.ReplaceAllFunc([]byte(source), func(found []byte) []byte {
+		variableName := toVariableName(string(found))
+		if variable, ok := variables[variableName]; ok {
+			return []byte(variable)
+		}
+		return found
+	})), nil
+}
+
+func toVariableName(exp string) string {
+	return exp[:len(exp)-1][2:]
 }
 
 func replaceTemplateExpression(template string) string {
--- openshift/process_test.go
+++ openshift/process_test.go
@@ -26,6 +26,28 @@ var processTemplate = `
       version: 1.0.58
       group: io.fabric8.online.packages
     name: ${PROJECT_NAME}
+    credentials.xml.tpl: |-
+      <?xml version='1.0' encoding='UTF-8'?>
+      <com.cloudbees.plugins.credentials.SystemCredentialsProvider plugin="credentials@1.23">
+      </com.cloudbees.plugins.credentials.SystemCredentialsProvider>
+`
+
+var processTemplateVariables = `
+- apiVersion: v1
+  kind: Project
+  metadata:
+    labels:
+      provider: fabric8
+      project: fabric8-online-team-environments
+      version: 1.0.58
+      group: io.fabric8.online.packages
+    credentials.xml.tpl: |-
+      <?xml version='1.0' encoding='UTF-8'?>
+      <com.cloudbees.plugins.credentials.SystemCredentialsProvider plugin="credentials@1.23">
+        <domainCredentialsMap class="hudson.util.CopyOnWriteMap$Hash">
+          ${KUBERNETES_CREDENTIALS}
+        </domainCredentialsMap>
+      </com.cloudbees.plugins.credentials.SystemCredentialsProvider>
 `
 
 func TestProcess(t *testing.T) {
@@ -43,7 +65,6 @@ func TestProcess(t *testing.T) {
 
 	t.Run("verify no template markers in output", func(t *testing.T) {
 		assert.False(t, regexp.MustCompile(`\${([A-Z_]+)}`).MatchString(proccsed))
-		assert.False(t, regexp.MustCompile(`{{([A-Z_]+)}}`).MatchString(proccsed))
 	})
 	t.Run("verify markers were replaced", func(t *testing.T) {
 		assert.Contains(t, proccsed, vars["PROJECT_DESCRIPTION"], "missing")
@@ -55,4 +76,20 @@ func TestProcess(t *testing.T) {
 		assert.Contains(t, proccsed, `'{"kind":"OAuthRedirectReference","apiVersion":"v1","reference":{"kind":"Route","name":"jenkins"}}'`)
 	})
 
+	t.Run("Verify not escaping xml/html values", func(t *testing.T) {
+		assert.Contains(t, proccsed, `<?xml version='1.0' encoding='UTF-8'?>`)
+	})
+}
+
+func TestProcessVariables(t *testing.T) {
+	vars := map[string]string{}
+
+	proccsed, err := openshift.Process(processTemplateVariables, vars)
+	require.Nil(t, err, "error processing template")
+
+	fmt.Println(proccsed)
+
+	t.Run("Verify non replaced markers are left", func(t *testing.T) {
+		assert.Contains(t, proccsed, "${KUBERNETES_CREDENTIALS}", "missing")
+	})
 }
