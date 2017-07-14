4c988cdad1b1cf6a59a5c59c468e43723d283e49
James Strachan <james.strachan@gmail.com>
2017-07-14 15:13:16 +0100

    fix failing test

--- template/generate_test.go
+++ template/generate_test.go
@@ -28,8 +28,8 @@ func TestFoundJenkins(t *testing.T) {
 	if !ok {
 		t.Fatalf("parameters not found")
 	}
-	if len(params) != 4 {
-		t.Fatalf("unknown number of parameters. found %v expected 4", len(params))
+	if len(params) != 5 {
+		t.Fatalf("unknown number of parameters. found %v expected 5", len(params))
 	}
 }
 
