1e712d9d8de7bd2078722330a41ae634dcba67a5
Xavier Coulon <xcoulon@redhat.com>
2018-02-13 15:09:39 +0100

    Remove k8s specific code (for now) (#526)
    * Remove 'auth' endpoint
    * Remove unused init tenant functions
    * Remove support for loading k8s templates
    
    Fixes openshiftio/openshift.io#1906
    
    Signed-off-by: Xavier Coulon <xcoulon@redhat.com>

--- template/generate.go
+++ template/generate.go
@@ -1,17 +1,11 @@
-//go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-che-mt/$CHE_VERSION/fabric8-tenant-che-mt-$CHE_VERSION-k8s-template.yml > fabric8-tenant-che-mt-kubernetes.yml"
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-che-mt/$CHE_VERSION/fabric8-tenant-che-mt-$CHE_VERSION-openshift.yml > fabric8-tenant-che-mt-openshift.yml"
 
-//go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-che/$CHE_VERSION/fabric8-tenant-che-$CHE_VERSION-k8s-template.yml > fabric8-tenant-che-kubernetes.yml"
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-che/$CHE_VERSION/fabric8-tenant-che-$CHE_VERSION-openshift.yml > fabric8-tenant-che-openshift.yml"
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-che-quotas-oso/$CHE_VERSION/fabric8-tenant-che-quotas-oso-$CHE_VERSION-openshift.yml > fabric8-tenant-che-quotas-oso-openshift.yml"
 
-//go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-jenkins/$JENKINS_VERSION/fabric8-tenant-jenkins-$JENKINS_VERSION-k8s-template.yml > fabric8-tenant-jenkins-kubernetes.yml"
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-jenkins/$JENKINS_VERSION/fabric8-tenant-jenkins-$JENKINS_VERSION-openshift.yml > fabric8-tenant-jenkins-openshift.yml"
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-jenkins-quotas-oso/$JENKINS_VERSION/fabric8-tenant-jenkins-quotas-oso-$JENKINS_VERSION-openshift.yml > fabric8-tenant-jenkins-quotas-oso-openshift.yml"
 
 //go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-team/$TEAM_VERSION/fabric8-tenant-team-$TEAM_VERSION-openshift.yml > fabric8-tenant-team-openshift.yml"
-//go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/tenant/packages/fabric8-tenant-team/$TEAM_VERSION/fabric8-tenant-team-$TEAM_VERSION-k8s-template.yml > fabric8-tenant-team-kubernetes.yml"
-
-//go:generate sh -c "curl http://central.maven.org/maven2/io/fabric8/apps/exposecontroller-app/$EXPOSCONTROLLER_VERSION/exposecontroller-app-$EXPOSCONTROLLER_VERSION-k8s-template.yml > fabric8-tenant-expose-kubernetes.yml"
 
 package template
