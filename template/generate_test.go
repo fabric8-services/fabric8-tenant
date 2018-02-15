fc78bcf022d2f05dd8e94c7156e0e23ae9f75e5c
Aslak Knutsen <aslak@4fs.no>
2018-02-15 20:36:56 +0100

    Update Tenant Jenkins v 4.0.53
    * Unbork borked pipeline

--- template/generate_test.go
+++ template/generate_test.go
@@ -34,7 +34,7 @@ func TestFoundJenkins(t *testing.T) {
 		t.Fatalf("parameters not found")
 	}
 
-	assert.Equal(t, 6, len(params), "unknown number of parameters")
+	assert.Equal(t, 7, len(params), "unknown number of parameters")
 }
 func TestFoundJenkinsKnownTypes(t *testing.T) {
 	verifyKindMapping(t, "template/fabric8-tenant-jenkins-openshift.yml")
