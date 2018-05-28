0337b0d217e5f35d650073f439ac194ec37a83ec
Suraj Deshmukh <surajd.service@gmail.com>
2018-05-28 12:46:08 +0530

    temporarily remove all the migration tests

--- migration/sql-test-files/005-add-data-to-tenants-oso-username.sql
+++
@@ -1,8 +0,0 @@
--- tenants
-INSERT INTO
-    tenants(created_at, updated_at, id, email, profile, oso_username)
-VALUES
-    (
-        now(), now(), 'e1452f0a-3e2f-419c-aa4f-46937fbead74', 'osio-developer1@email.com', 'free', 'userRandom'
-    )
-;
