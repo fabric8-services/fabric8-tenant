d84fbd8955bf26f69756c2bafe83fde9ce016d29
Matous Jobanek <mjobanek@redhat.com>
2018-09-19 10:47:08 +0200

    chore: move GetPublicKeys method to auth/service.go file

--- auth/service_test.go
+++ auth/service_test.go
@@ -159,3 +159,28 @@ func TestUserProfileClient_GetUserCluster(t *testing.T) {
 		})
 	}
 }
+
+func TestPublicKeys(t *testing.T) {
+
+	t.Run("valid keys", func(t *testing.T) {
+		//given
+		authService, r, err := testdoubles.NewAuthClientService("../test/data/token/auth_get_keys", "http://authservice")
+		require.NoError(t, err)
+		defer r.Stop()
+		// when
+		result, err := authService.GetPublicKeys()
+		// then
+		require.NoError(t, err)
+		assert.Len(t, result, 3)
+	})
+
+	t.Run("invalid url", func(t *testing.T) {
+		//given
+		authService, _, err := testdoubles.NewAuthClientService("", "http://google.com")
+		assert.NoError(t, err)
+		// when
+		_, err = authService.GetPublicKeys()
+		// then
+		assert.Error(t, err)
+	})
+}
