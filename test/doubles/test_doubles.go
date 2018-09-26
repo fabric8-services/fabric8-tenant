995977388820d2a8bfa98b532315e2fce3d3991a
Matous Jobanek <mjobanek@redhat.com>
2018-09-26 14:12:25 +0200

    chore: set capacity and not size of a slice

--- test/doubles/test_doubles.go
+++ test/doubles/test_doubles.go
@@ -11,7 +11,7 @@ type Environment struct {
 }
 
 func SetEnvironments(environments ...Environment) func() {
-	originalValues := make([]Environment, len(environments))
+	originalValues := make([]Environment, 0, len(environments))
 
 	for _, env := range environments {
 		originalValues = append(originalValues, Env(env.key, os.Getenv(env.key)))
