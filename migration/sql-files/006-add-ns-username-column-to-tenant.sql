139d17823be966263727fc78e9fc0a4f4c09b926
Matous Jobanek <matousjobanek@gmail.com>
2018-11-01 18:17:49 +0100

    add migration that sets ns_username in existing tenants

--- migration/sql-files/006-add-ns-username-column-to-tenant.sql
+++ migration/sql-files/006-add-ns-username-column-to-tenant.sql
@@ -1,2 +1,3 @@
 ALTER TABLE tenants ADD COLUMN ns_username TEXT;
 CREATE INDEX idx_ns_username ON tenants USING btree (ns_username);
+UPDATE tenants t SET t.ns_username = (SELECT name FROM namespaces n WHERE t.id = n.tenant_id AND t.type = 'user');
