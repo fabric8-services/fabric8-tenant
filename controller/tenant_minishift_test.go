3c0ddca1076a1a4f1f712b89be95ce6af85aaed5
Matous Jobanek <mjobanek@redhat.com>
2018-12-12 14:26:55 +0000

    fix name of tenant minishift test

--- controller/tenant_minishift_test.go
+++ controller/tenant_minishift_test.go
@@ -24,7 +24,7 @@ type TenantControllerMinishiftTestSuite struct {
 	minishift.TestSuite
 }
 
-func TestTenantControllerWithMinisshift(t *testing.T) {
+func TestTenantControllerWithMinishift(t *testing.T) {
 	suite.Run(t, &TenantControllerMinishiftTestSuite{
 		TestSuite: minishift.TestSuite{DBTestSuite: gormsupport.NewDBTestSuite("../config.yaml")}})
 }
